8bd4638f2e3737239b0347047f946e57f1303885
Thomas Froud <thomasfroud10@gmail.com>
2025-11-27 20:32:24 +0000

    Initial project creation

---
+++ src/main/java/com/CodeerStudios/chatEvents/ChatEvents.java
@@ -0,0 +1,17 @@
+package com.CodeerStudios.chatEvents;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChatEvents extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
